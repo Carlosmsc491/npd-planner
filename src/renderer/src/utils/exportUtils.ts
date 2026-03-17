506d3f1818558d96f9c80e31907e247f274f4209
Carlosmsc491 <carlosmsc491@gmail.com>
2026-03-16 23:48:49 -0400

    fix: Windows Electron main process

--- src/renderer/src/utils/exportUtils.ts
+++
@@ -1,4 +0,0 @@
-// src/renderer/src/utils/exportUtils.ts
-// Re-export from utils.ts
-
-export { exportSummaryToCSV } from './utils'
--- src/renderer/src/utils/hashUtils.ts
+++
@@ -1,4 +0,0 @@
-// src/renderer/src/utils/hashUtils.ts
-// Re-export from utils.ts
-
-export { hashSHA256 } from './utils'
